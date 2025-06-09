7f0b8508d9dbdef499b311876aaa19ba6a1bdb7b
Stomatologoanatom <exactevildoer@mail.ru>
2025-06-09 20:54:08 +0300

    Update Telegram_bot.py

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -34,11 +34,11 @@ except ValueError:
 
 # --- Темы и соответствующие файлы ---
 THEMES = {
-    "🏳️‍⚧️ Европа": "Topics/Europe.txt",
-    "🍜 Азия": "Topics/Asia.txt",
-    "💵 Америка": "Topics/America.txt",
-    "👳‍♂️✈🏢🏢 Ближний Восток": "Topics/MiddleEast.txt",
-    "εつ▄█▀█● НАТО": "Topics/NATO.txt"
+    "🏳️‍⚧️ Европа": "Europe.txt",
+    "🍜 Азия": "Asia.txt",
+    "💵 Америка": "America.txt",
+    "👳‍♂️✈🏢🏢 Ближний Восток": "MiddleEast.txt",
+    "εつ▄█▀█● НАТО": "NATO.txt"
 }
 
 # --- Главное меню ---
